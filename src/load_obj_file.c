348815eae14c2e01d7794bd8775f56d94609e378
gedemais <geoffrey.demaiso@gmail.com>
2022-03-24 03:24:02 +0100

    started to comment

--- src/load_obj_file.c
+++ src/load_obj_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+// Constant array of strings used to store obj format line identifiers.
 static const char		*obj_lines_ids[OBJ_MAX] = {	[OBJ_COMMENT] = "#",
 											[OBJ_MTLLIB] = "mtllib",
 											[OBJ_OBJECT_NAME] = "o",
@@ -7,7 +8,10 @@ static const char		*obj_lines_ids[OBJ_MAX] = {	[OBJ_COMMENT] = "#",
 											[OBJ_USEMTL] = "usemtl",
 											[OBJ_FACE] = "f",
 											[OBJ_SMOOTH_SHADING] = "s"};
-
+/*
+ * Globals keeping track of the index of the mesh we are currently
+ * loading, and of which material is applied for currently loaded face.
+ */
 static int16_t	used_mtl = SHRT_MAX;
 static int32_t	current_mesh = INT_MAX;
 
@@ -75,6 +79,13 @@ static unsigned char	obj_usemtl_loader(t_env *env, char **tokens)
 	return (ERR_NONE);
 }
 
+static unsigned char	assign_face_indexes(t_face *new, char **tokens, int indexes[3])
+{
+	new->a = (int32_t)ft_atoi(tokens[indexes[0]]) - 1;
+	new->b = (int32_t)ft_atoi(tokens[indexes[1]]) - 1;
+	new->c = (int32_t)ft_atoi(tokens[indexes[2]]) - 1;
+}
+
 static unsigned char	check_face_indexes(t_env *env, t_face new)
 {
 	if (new.a < 0 || new.a >= env->scene.vertexs.nb_cells
@@ -88,64 +99,64 @@ static unsigned char	triangulation(t_env *env, t_mesh *parent, uint32_t a_index,
 {
 	t_face	news[2];
 
-	if (nb_vertexs == 4) // Quad -> 2 triangles
+	if (nb_vertexs == 4) // Quad -> 2 polygons
 	{
 		uint32_t	b_index = a_index + 1;
-		news[0].a = (int32_t)ft_atoi(tokens[1]) - 1;
-		news[0].b = (int32_t)ft_atoi(tokens[2]) - 1;
-		news[0].c = (int32_t)ft_atoi(tokens[3]) - 1;
+		// Polygon faces indexes assignment
+		assign_face_indexes(&news[0], tokens, (int[3]){1, 2, 3});
+		assign_face_indexes(&news[1], tokens, (int[3]){1, 3, 4});
 
-		news[1].a = (int32_t)ft_atoi(tokens[1]) - 1;
-		news[1].b = (int32_t)ft_atoi(tokens[3]) - 1;
-		news[1].c = (int32_t)ft_atoi(tokens[4]) - 1;
+		// Checks indexes values
+		if (check_face_indexes(env, news[0]) != ERR_NONE
+			|| check_face_indexes(env, news[1]) != ERR_NONE)
+			return (ERR_INVALID_VERTEX_INDEX_FOR_FACE);
 
+		// Moves new faces into pool
 		if (push_dynarray(&env->scene.faces, &news[0], false)
 			|| push_dynarray(&env->scene.faces, &news[1], false))
 			return (ERR_MALLOC_FAILED);
 
+		// Moves faces indexes into parent mesh
 		if (push_dynarray(&parent->faces, &a_index, false)
 			|| push_dynarray(&parent->faces, &b_index, false))
 			return (ERR_MALLOC_FAILED);
-
-		if (check_face_indexes(env, news[0]) != ERR_NONE
-			|| check_face_indexes(env, news[1]) != ERR_NONE)
-			return (ERR_INVALID_VERTEX_INDEX_FOR_FACE);
 	}
 	return (ERR_NONE);
 }
 
 static unsigned char	obj_face_loader(t_env *env, char **tokens)
 {
-	t_face			new;
-	t_mesh			*parent;
-	unsigned short	nb_vertexs;
-	uint32_t		face_index;
+	t_face			new; // New face instance
+	t_mesh			*parent; // Mesh composed by the face
+	unsigned short	nb_vertexs; // Number of vertices composing the face
+	uint32_t		face_index; // Index of the instance in the faces pool
 
 	parent = dyacc(&env->scene.meshs, (int)current_mesh);
 	nb_vertexs = (unsigned short)ft_tablen(tokens) - 1;
 
-	if (env->scene.faces.c == NULL
+	if (env->scene.faces.c == NULL // Initialization of faces pool
 		&& init_dynarray(&env->scene.faces, sizeof(t_face), 256))
 		return (ERR_MALLOC_FAILED);
 
 	face_index = (uint32_t)env->scene.faces.nb_cells;
 
-	if (nb_vertexs == 3)
+	if (nb_vertexs == 3) // If the face is a polygon
 	{
 		new.a = (int32_t)ft_atoi(tokens[1]) - 1;
 		new.b = (int32_t)ft_atoi(tokens[2]) - 1;
 		new.c = (int32_t)ft_atoi(tokens[3]) - 1;
 		new.mtl = used_mtl;
-
+		// Moves instance in the pool.
 		if (push_dynarray(&env->scene.faces, &new, false)
-			|| push_dynarray(&parent->faces, &face_index, false))
+			// Moves instance's pool index in the parent mesh.
+			|| push_dynarray(&parent->faces, &face_index, false)) 
 			return (ERR_MALLOC_FAILED);
-
+		// Negative / pool bound test.
 		return (check_face_indexes(env, new));
 	}
-	else if (nb_vertexs < 5)
+	else if (nb_vertexs < 5) // Transforms non polygon faces to polygons, and moves them in the faces pool
 		return (triangulation(env, parent, face_index, tokens, nb_vertexs));
-
+	// If this kind of face is not handled
 	return (ERR_INVALID_PRIMITIVE_DESCRIPTION);
 }
 
@@ -153,8 +164,9 @@ static unsigned char	obj_face_loader(t_env *env, char **tokens)
 
 static unsigned char	obj_loader(t_env *env, char *line)
 {
+	// Function pointer array used to bind a function to each line identifier.
 	static unsigned char	(*obj_loading_fts[OBJ_MAX])(t_env *, char **) = {
-											[OBJ_COMMENT] = NULL,
+											[OBJ_COMMENT] = NULL, // NULL-set members are not handled
 											[OBJ_MTLLIB] = obj_mtllib_loader,
 											[OBJ_OBJECT_NAME] = obj_object_name_loader,
 											[OBJ_VERTEX] = obj_vertex_loader,
@@ -163,17 +175,20 @@ static unsigned char	obj_loader(t_env *env, char *line)
 											[OBJ_SMOOTH_SHADING] = NULL};
 	char				**tokens;
 
+	// Split the line in tokens by whitespaces.
 	if (!(tokens = ft_strsplit(line, "\b\t\v\f\r ")))
 		return (ERR_MALLOC_FAILED);
 
+	// Identifies line type and lauches the corresponding loader.
 	for (unsigned int i = 0; i < OBJ_MAX; i++)
 		if (ft_strcmp(tokens[0], obj_lines_ids[i]) == 0)
 		{
-			if ((i == OBJ_VERTEX || i == OBJ_FACE) && current_mesh == INT_MAX)
+			// Security check, if no objects are declared, then we can't add vertex or face.
+			if ((i == OBJ_VERTEX || i == OBJ_FACE) && current_mesh == INT_MAX) // Initial value check
 				return (ERR_NO_DEFINED_OBJECT);
 			return (obj_loading_fts[i] ? obj_loading_fts[i](env, tokens) : ERR_NONE);
 		}
-
+	// Free tokens strings array
 	ft_free_ctab(tokens);
 	return (ERR_INVALID_OBJ_LINE_ID);
 }
@@ -183,9 +198,11 @@ unsigned char			load_obj_file(t_env *env, char *path)
 	char			**lines;
 	unsigned char	code;
 
+	// Maps the .obj file in memory, then splits it into lines to parse it easier.
 	if ((code = readlines(path, &lines)) != ERR_NONE)
 		return (code);
 
+	// Iterate through lines to load obj data.
 	for (unsigned int i = 0; lines[i]; i++)
 		if ((code = obj_loader(env, lines[i])))
 			return (code);
